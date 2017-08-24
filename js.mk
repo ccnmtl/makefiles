820e3aca0fad69434caecf6ee9bb4dc24d9c68a2
Susan Dreher <sdreher@columbia.edu>
2017-08-24 15:39:41 -0400

    deprecating jshint & jscs in favor of eslint

--- js.mk
+++ js.mk
@@ -1,6 +1,7 @@
 # VERSION=1.2.0
 
 # CHANGES:
+# 1.3.0 - restore eslint
 # 1.2.0 - restore jshint/jscs
 
 # expect JS_FILES to be set from the main Makefile, but default
@@ -19,8 +20,6 @@ JS_FILES ?= media/js
 
 NODE_MODULES ?= ./node_modules
 JS_SENTINAL ?= $(NODE_MODULES)/sentinal
-JSHINT ?= $(NODE_MODULES)/jshint/bin/jshint
-JSCS ?= $(NODE_MODULES)/jscs/bin/jscs
 ESLINT ?= $(NODE_MODULES)/.bin/eslint
 
 $(JS_SENTINAL): package.json
@@ -28,16 +27,10 @@ $(JS_SENTINAL): package.json
 	npm install
 	touch $(JS_SENTINAL)
 
-jshint: $(JS_SENTINAL)
-	$(JSHINT) $(JS_FILES)
-
-jscs: $(JS_SENTINAL)
-	$(JSCS) $(JS_FILES)
-
 eslint: $(JS_SENTINAL)
 	$(ESLINT) $(JS_FILES)
 
 jstest: $(JS_SENTINAL)
 	npm test
 
-.PHONY: jshint jscs eslint jstest
+.PHONY: eslint jstest
