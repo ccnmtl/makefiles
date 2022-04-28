d13fd5d5dcece1511d9227d50cb9093609a5dcb1
Susan Dreher <sdreher@gmail.com>
2022-04-28 14:59:32 -0400

    Update version & changelog

--- django.mk
+++ django.mk
@@ -1,10 +1,11 @@
-# VERSION=1.8.0
+# VERSION=1.9.0
 
 # CHANGES:
 # 1.9.0              - Use coverage tool directly to generate coverage
 #                      reports.
 #                    - wheel and pip updates
 #                    - Use pre-compiled binary wheel for cryptography
+#                    - Remove Travis references
 # 1.8.0 - 2019-10-21 - Don't run flake8 on local_settings.py
 # 1.7.0 - 2018-05-31 - Now using python 3 by default
 #                    - Removed virtualenv.py in favor of python 3's
